490738b61b0735c1c77d06b2c423bbd9f613c9e9
emon3455 <emon.mhk69@gmail.com>
2023-01-31 01:31:37 +0600

    finding maximum number

---
+++ problem0.js
@@ -0,0 +1,17 @@
+// getting maximum number from 3 number:
+
+let num1 =50;
+let num2 = 990;
+let num3 = 90;
+
+if(num1>num2 && num1 > num3){
+    console.log("Maximum number:",num1);
+}
+
+else if(num2 > num3 && num2 > num1){
+    console.log("Maximum number:",num2);
+}
+
+else{
+    console.log("Maximum number:",num3);
+}

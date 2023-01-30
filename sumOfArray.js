45635333462b00b1ce8cf57c8694650d391d9b03
emon3455 <emon.mhk69@gmail.com>
2023-01-30 23:31:37 +0600

    sum of an array all element

---
+++ sumOfArray.js
@@ -0,0 +1,15 @@
+function sumOfAllElement(ar){
+    let sum =0;
+    console.log("Index "+" Value"+"    Sum");
+
+    for(let i=0; i < ar.length; i++){
+        sum = sum + ar[i];
+        console.log(i,"\t",ar[i],"\t",sum);
+    }
+
+    return sum;
+}
+
+let ar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = sumOfAllElement(ar);
+console.log("Sum:",sum);

c0219563ec6bfe7cff1bea3730c1efcae505748c
emon3455 <emon.mhk69@gmail.com>
2023-01-30 22:24:20 +0600

    check for even number

---
+++ checkForEvenNumber.js
@@ -0,0 +1,14 @@
+function isEven(number){
+    if(number%2 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+let num1 = isEven(222);
+console.log(num1);
+
+let num2 = isEven(133);
+console.log(num2);
